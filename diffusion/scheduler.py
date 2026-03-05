2a1860dcbe219a96ed7678d05eddb1b26ece31b1
Suyash647 <b24ee1078@iitj.ac.in>
2026-03-06 00:38:51 +0530

    Add linear noise scheduler for baseline

--- diffusion/scheduler.py
+++ diffusion/scheduler.py
@@ -1,16 +1,11 @@
 import torch
 
 
-class DiffusionScheduler:
-    def __init__(self, timesteps=1000, beta_start=1e-4, beta_end=0.02):
-        self.timesteps = timesteps
+def get_noise_schedule(T, beta_start=1e-4, beta_end=0.02):
+    """
+    Linear beta schedule used in the original DDPM paper
+    """
 
-        self.beta = torch.linspace(beta_start, beta_end, timesteps)
-        self.alpha = 1.0 - self.beta
-        self.alpha_bar = torch.cumprod(self.alpha, dim=0)
+    betas = torch.linspace(beta_start, beta_end, T)
 
-    def to(self, device):
-        self.beta = self.beta.to(device)
-        self.alpha = self.alpha.to(device)
-        self.alpha_bar = self.alpha_bar.to(device)
-        return self
+    return betas
